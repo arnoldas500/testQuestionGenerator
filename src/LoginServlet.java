4e85feb76619c7246d26a658625c2a4aa13a02cf
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2017-10-02 17:04:51 -0400

    adding login page to registration

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,8 +1,13 @@
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -65,87 +70,199 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	
+	/*
+	try {
+		//fetching the data and storing in local variable 
+		String firstName = request.getParameter("firstName");
+		String lastname = request.getParameter("lastname");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String role = request.getParameter("role");
+		//creating sql queery from above data
+		//user is the actual sql table name and name and password is what it takes
+		String sql = "insert into User(username,password,role) values(?,?,?)";
+		String sql2 = "insert into User_Profile(firstName,lastname) values(?,?)";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/portal";
+		String usernameSQL = "root";
+		String passwordSQL = "";
+		Connection connection = DriverManager.getConnection(url,usernameSQL,passwordSQL);
+		
+		//execute sql command for users table
+		try(PreparedStatement ps = connection.prepareStatement(sql)){
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ps.setString(3, role);
+			//now need to execute this statement
+			ps.executeUpdate();
+		}
+		
+		//execute sql command for users profile table
+		try(PreparedStatement ps2 = connection.prepareStatement(sql2)){
+			ps2.setString(1, firstName);
+			ps2.setString(2, lastname);
+			//now need to execute this statement
+			ps2.executeUpdate();
+		}
+		
+		try (PreparedStatement stmt = connection.prepareStatement("INSERT INTO PUBLISHER (CODE, PUBLISHER_NAME) VALUES (?, ?)")) {
+	        stmt.setString(1, book.getPublisher().getCode());   
+	        stmt.setString(2, book.getPublisher().getName());           
+	        stmt.executeUpdate();
+	    }
+	   
+		
+		
+		//print out what was executed
+		PrintWriter out = response.getWriter();
+		out.println("You have successfully signed up!");
+		//***DONT FORGET TO UPDATE WEB.XML under WEB-INF
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	*/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// this is where control will go from the jsp login post method
-		// this is where i check if the username and password are valid or not
-		// doGet(request, response);
-		// will get our information from the request and by specifying the name of what
-		// we want to get ex username
-		String username = request.getParameter("username");
-		String usernameUpper = username.toUpperCase();
-		String password = request.getParameter("password");
-		String roles = request.getParameter("roles");
-		if (usernameUpper.equals("ICSI518") && password.equals("Fall2017")) {
-			// if login is successfull then redirect to members jsp page
-			// request.getSession().removeAttribute("errorMessage");
-			// response.sendRedirect("members.jsp");
-			if (roles.equals("Supplier")) {
-				request.getSession().setAttribute("currentUser", username);
-				request.getRequestDispatcher("Supplier.jsp").forward(request, response);
-				// request.setAttribute("currentDate", "Please enter username.");
-			}
-
-			if (roles.equals("Wholesaler")) {
-				request.getSession().setAttribute("currentUser", username);
-				request.getRequestDispatcher("Wholesaler.jsp").forward(request, response);
-			}
-			
-			if (roles.equals("Retailer")) {
-				request.getSession().setAttribute("currentUser", username);
-				request.getRequestDispatcher("Retailer.jsp").forward(request, response);
-			}
-			
-			if (roles.equals("Customer")) {
-				request.getSession().setAttribute("currentUser", username);
-				request.getRequestDispatcher("Customer.jsp").forward(request, response);
-			}
-
-			// request.getRequestDispatcher("members.jsp").forward(request, response);
-		} else { // if the username or password incorrect will go here
-			// if not succesfull redirect to error.jsp
-			// response.sendRedirect("error.jsp");
-			// request.getSession().removeAttribute("errorMessage");
-			if (usernameUpper.isEmpty() && password.isEmpty()) {
-				request.setAttribute("errorMessage", "Please enter username and password.");
-			}
-			 else if (usernameUpper.isEmpty()) {
-				request.setAttribute("errorMessage", "Please enter username.");
-			} else if (password.isEmpty()) {
-				request.setAttribute("errorMessage", "Please enter password.");
-			} else {
-				request.setAttribute("errorMessage", "Invalid username and password.");
-			}
+		
+		try {
 			
 			/*
-			***TESTING to see if random input and some field left blank***
-			{ // if the username or password incorrect will go here
-			// if not succesfull redirect to error.jsp
-			// response.sendRedirect("error.jsp");
-			// request.getSession().removeAttribute("errorMessage");
-			if (usernameUpper.equals("ICSI518") && password.isEmpty()) {
-				request.setAttribute("errorMessage", "Please enter password.");
-			} else if (usernameUpper.isEmpty() && password.equals("Fall2017")) {
-				request.setAttribute("errorMessage", "Please enter username.");
-			} else if (usernameUpper.isEmpty() && password.isEmpty()) {
-				request.setAttribute("errorMessage", "Please enter username and password.");
-			} else {
-				request.setAttribute("errorMessage", "Invalid username and password.");
-			}
-			
-			
-			
-			*/
-			
-			/*
-			 * if(!(username.equals("ICSI518")) && !(password.equals("Fall2017"))) {
-			 * request.getSession().setAttribute("errorMessage",
-			 * "Invalid username and password."); }
+			 * 
+			 * 
 			 */
-			// request.getSession().setAttribute("errorMessage", "Please enter username and
-			// password");
-			request.getRequestDispatcher("Login.jsp").forward(request, response);
+			
+			// this is where control will go from the jsp login post method
+			// this is where i check if the username and password are valid or not
+			// doGet(request, response);
+			// will get our information from the request and by specifying the name of what
+			// we want to get ex username
+			String username = request.getParameter("username");
+			String usernameUpper = username.toUpperCase();
+			String password = request.getParameter("password");
+			String roles = request.getParameter("roles");
+			String dbName = null;
+			String dbPassword = null;
+			String dbRole = null;
+			
+			//creating sql queery from above data to fetch the data from the mySQL database
+			String sql = "select * from User where username=? and password=? and role=?";
+			//String sql2 = "insert into User_Profile(firstName,lastname) values(?,?)";
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String url = "jdbc:mysql://localhost:3306/portal";
+			String usernameSQL = "root";
+			String passwordSQL = "";
+			Connection connection = DriverManager.getConnection(url,usernameSQL,passwordSQL);
+			
+			//execute sql command for users table
+			try(PreparedStatement ps = connection.prepareStatement(sql)){
+				ps.setString(1, username);
+				ps.setString(2, password);
+				ps.setString(3, roles);
+				//fetch the data and store it somewhere 
+				ResultSet resultSet = ps.executeQuery();
+				PrintWriter out = response.getWriter();
+				while(resultSet.next()) {
+					dbName = resultSet.getString(2);
+					dbPassword = resultSet.getString("password");
+					dbRole = resultSet.getString("role");
+				}
+				//checking if information actually exist in the database
+				if(username.equals(dbName) && password.equals(dbPassword) && roles.equals(dbRole)) {
+					out.println("You have successfully signed in!");
+				}else {
+					//response.sendRedirect("login.jsp");
+					RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+					rd.include(request, response); //could also use forward instead of include
+				}
+			}
+			
+			if (usernameUpper.equals("ICSI518") && password.equals("Fall2017")) {
+				// if login is successfull then redirect to members jsp page
+				// request.getSession().removeAttribute("errorMessage");
+				// response.sendRedirect("members.jsp");
+				if (roles.equals("Supplier")) {
+					request.getSession().setAttribute("currentUser", username);
+					request.getRequestDispatcher("Supplier.jsp").forward(request, response);
+					// request.setAttribute("currentDate", "Please enter username.");
+				}
+
+				if (roles.equals("Wholesaler")) {
+					request.getSession().setAttribute("currentUser", username);
+					request.getRequestDispatcher("Wholesaler.jsp").forward(request, response);
+				}
+				
+				if (roles.equals("Retailer")) {
+					request.getSession().setAttribute("currentUser", username);
+					request.getRequestDispatcher("Retailer.jsp").forward(request, response);
+				}
+				
+				if (roles.equals("Customer")) {
+					request.getSession().setAttribute("currentUser", username);
+					request.getRequestDispatcher("Customer.jsp").forward(request, response);
+				}
+
+				// request.getRequestDispatcher("members.jsp").forward(request, response);
+			} else { // if the username or password incorrect will go here
+				// if not succesfull redirect to error.jsp
+				// response.sendRedirect("error.jsp");
+				// request.getSession().removeAttribute("errorMessage");
+				if (usernameUpper.isEmpty() && password.isEmpty()) {
+					request.setAttribute("errorMessage", "Please enter username and password.");
+				}
+				 else if (usernameUpper.isEmpty()) {
+					request.setAttribute("errorMessage", "Please enter username.");
+				} else if (password.isEmpty()) {
+					request.setAttribute("errorMessage", "Please enter password.");
+				} else {
+					request.setAttribute("errorMessage", "Invalid username and password.");
+				}
+				
+				/*
+				***TESTING to see if random input and some field left blank***
+				{ // if the username or password incorrect will go here
+				// if not succesfull redirect to error.jsp
+				// response.sendRedirect("error.jsp");
+				// request.getSession().removeAttribute("errorMessage");
+				if (usernameUpper.equals("ICSI518") && password.isEmpty()) {
+					request.setAttribute("errorMessage", "Please enter password.");
+				} else if (usernameUpper.isEmpty() && password.equals("Fall2017")) {
+					request.setAttribute("errorMessage", "Please enter username.");
+				} else if (usernameUpper.isEmpty() && password.isEmpty()) {
+					request.setAttribute("errorMessage", "Please enter username and password.");
+				} else {
+					request.setAttribute("errorMessage", "Invalid username and password.");
+				}
+				
+				
+				
+				*/
+				
+				/*
+				 * if(!(username.equals("ICSI518")) && !(password.equals("Fall2017"))) {
+				 * request.getSession().setAttribute("errorMessage",
+				 * "Invalid username and password."); }
+				 */
+				// request.getSession().setAttribute("errorMessage", "Please enter username and
+				// password");
+				request.getRequestDispatcher("Login.jsp").forward(request, response); //could also use include 
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 
 	}
 
